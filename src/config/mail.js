4876e165c313ca4c7ba5a280a6189be9e7338fe0
carlosberti <carlosberti1@hotmail.com>
2019-08-06 15:04:30 -0300

    GoBarber BackEnd

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  default: {
+    from: 'Carlos Augusto <carlosberti1@hotmail.com>',
+  },
+};
